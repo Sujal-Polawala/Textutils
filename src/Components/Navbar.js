4ecafd6b863d639b5959247bf54f6aa1f0b369f1
Sujal-Polawala <sujalpolawala@gmail.com>
2024-05-20 12:29:12 +0530

    Add Navbar.js

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import PropTypes from 'prop-types'
+// import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 
 export default function Navbar(props) {
@@ -11,24 +11,23 @@ export default function Navbar(props) {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+                <Link className="nav-link" aria-current="page" to="/">Home</Link>
               </li>
               <li className="nav-item">
                 <Link className="nav-link" to="/about">About</Link>
               </li>
             </ul>
-            <form className="d-flex"> 
-              <div className={`container form-check form-switch text-${props.mode==='light'?'dark':'light'}`}> 
-                <input className="form-check-input" onClick={props.toggleMode} type="checkbox" role="switch" id="flexSwitchCheckDefault" />
-                <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Enable Dark Mode</label>
-              </div>
-              {/* <div className={`container form-check form-switch text-${props.mode==='light'?'green':'light'}`}> 
-                <input className="form-check-input" onClick={props.tmode} type="checkbox" role="switch" id="flexSwitchCheckDefault1" />
-                <label className="form-check-label" htmlFor="flexSwitchCheckDefault1">Enable Green Mode</label>
-              </div> */}
+            <div className="d-flex"> 
+              <div className="bg-primary rounded mx-2" onClick={()=> {props.toggleMode('primary')}} style={{height: '30px', width: '30px', cursor: 'pointer'}}></div>
+              <div className="bg-danger rounded mx-2" onClick={()=> {props.toggleMode('danger')}} style={{height: '30px', width: '30px', cursor: 'pointer'}}></div>
+              <div className="bg-success rounded mx-2" onClick={()=> {props.toggleMode('success')}} style={{height: '30px', width: '30px', cursor: 'pointer'}}></div>
+              <div className="bg-warning rounded mx-2" onClick={()=> {props.toggleMode('warning')}} style={{height: '30px', width: '30px', cursor: 'pointer'}}></div>
+              <div className="bg-light rounded mx-2" onClick={()=> {props.toggleMode('light')}} style={{height: '30px', width: '30px', cursor: 'pointer'}}></div>
+              <div className="bg-dark rounded mx-2" onClick={()=> {props.toggleMode('dark')}} style={{height: '30px', width: '30px', cursor: 'pointer'}}></div>
+
               {/* <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
               <button className="btn btn-outline-primary" type="submit">Search</button> */}
-            </form>
+            </div>
           </div>
         </div>
       </nav>
